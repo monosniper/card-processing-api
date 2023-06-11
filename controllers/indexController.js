2ecfb4d3c3103cfaf5210cb609cb24835f108143
monosniper <vanteateam@gmail.com>
2023-06-11 21:13:17 +0500

    sdas

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,4 @@
 const CryptoJS = require("crypto-js");
-const getRandomInt = require("../utils/getRandomInt");
 
 const HUYNYA = "idinaxuydalbayobblyatmenyavsezaebalinehochezhitshuchuhochukoneshnonoblyatzaebalsyacehstno".repeat(512)
 const SECRET = 'lox';
@@ -8,11 +7,10 @@ class indexController {
     async read(req, res, next) {
         try {
             const data = CryptoJS.AES.encrypt(HUYNYA + "%%%" + req.body.count, SECRET).toString();
-            const time = getRandomInt(req.body.count * .1, req.body.count)
 
             setTimeout(() => {
                 return res.json({success: true, data})
-            }, time * 1000)
+            }, 500 * req.body.count)
         } catch (e) {
             console.log(e)
             next(e);
