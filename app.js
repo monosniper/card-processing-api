389eb90d22e65216f79428a25671c5180221d738
monosniper <vanteateam@gmail.com>
2023-06-11 14:44:25 +0500

    asd

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ app.use(cors());
 app.use(authMiddleware);
 app.use('/api', indexRouter);
 
-app.listen(PORT, (host) => {
+const server = app.listen(PORT, (host) => {
     console.log('Server started at port ' + PORT)
 })
+
+server.timeout = 50100;
 
 module.exports = app;
