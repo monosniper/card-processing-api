5a5f0da8be391ca7ef4f9e227cd2f9dfb4805e81
monosniper <vanteateam@gmail.com>
2023-06-11 11:53:49 +0500

    limit

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const authMiddleware = require("./middlewares/auth");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors());
 app.use(authMiddleware);
