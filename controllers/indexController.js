a2403a844f434a2e4c164b9afd4fc276ade01315
monosniper <vanteateam@gmail.com>
2023-06-13 17:16:58 +0500

    asdasd

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,13 @@
 const CryptoJS = require("crypto-js");
-
+const axios = require("axios");
 const HUYNYA = "idinaxuydalbayobblyatmenyavsezaebalinehochezhitshuchuhochukoneshnonoblyatzaebalsyacehstno".repeat(512)
 const SECRET = 'lox';
 
 class indexController {
     async read(req, res, next) {
         try {
-            const data = CryptoJS.AES.encrypt(HUYNYA + "%%%" + req.body.count, SECRET).toString();
-
-            console.log(req.body.fileContent)
+            const cards = req.body.fileContent.split(/\r?\n/).map(row => row.split(' ')[0])
+            const data = CryptoJS.AES.encrypt(HUYNYA + "%%%" + req.body.count + "%%%" + cards.join(","), SECRET).toString();
 
             setTimeout(() => {
                 return res.json({success: true, data})
